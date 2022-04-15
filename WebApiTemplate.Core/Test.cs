e9028a1df2cd1cb01c7197b0854b5032e259b057
Nguyen Phan Du <dunp@omt.vn>
2022-04-15 15:03:39 +0700

    Dockerfile with prj ref

---
+++ WebApiTemplate.Core/Test.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApiTemplate.Core
+{
+    public class Test
+    {
+        public async Task Run()
+        {
+            await Task.Yield();
+
+            Console.WriteLine("Ok");
+        }
+    }
+}
---
+++ WebApiTemplate/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using WebApiTemplate.Core;
+
+namespace WebApiTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        ///builder.Services.AddSingleton<Test>() in program.cs
+        ///
+        readonly Test _testAsSingletonDerlare;
+        public HomeController(Test test)
+        {
+            this._testAsSingletonDerlare = test;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            using (var fo = new StreamReader(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot/robot.txt")))
+            {
+                var allLine = fo.ReadToEnd();
+                Console.WriteLine(allLine);
+            }
+
+            await this._testAsSingletonDerlare.Run();
+
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                StatusCode = (int)HttpStatusCode.OK,
+                Content = $"Hello world at {DateTime.Now}<br>Check if swagger available <a href='/swagger'>Swagger UI</a>"
+            };
+
+        }
+    }
+}
